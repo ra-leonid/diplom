bd7e4366017c844996bddf5c20d34750eaca1553
Leonid Rastorguev <rastorguyevl@list.ru>
2023-04-21 08:09:49 +0300

    Del backend "s3"

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "tfstate-diplom-devops"
-    region     = "ru-central1"
-    key        = "terraform.tfstate"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    endpoint   = "storage.yandexcloud.net"
+#    bucket     = "tfstate-diplom-devops"
+#    region     = "ru-central1"
+#    key        = "terraform.tfstate"
+#    skip_region_validation      = true
+#    skip_credentials_validation = true
+#  }
+#}
