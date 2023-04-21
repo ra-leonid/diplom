c7b3e8f47227812d0fd281985bdc5902bd73fb80
Leonid Rastorguev <rastorguyevl@list.ru>
2023-04-21 07:46:19 +0300

    Add backend "remote"

--- versions.tf
+++ versions.tf
@@ -5,4 +5,13 @@ terraform {
       version = "0.88.0"
     }
   }
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "ra-leonid"
+
+    workspaces {
+      #name = "terraform-cloud"
+      prefix = "diplom-"
+    }
+  }
 }
